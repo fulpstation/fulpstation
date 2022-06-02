56477e329fedd38389dd9b5f956983cad5c10ad2
JohnFulpWillard <53777086+JohnFulpWillard@users.noreply.github.com>
2022-06-02 04:28:42 -0400

    Updates TGU + re-adds unit test + Heliostation

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/antag_tips.tsx
+++
@@ -1,8 +0,0 @@
-import { CheckboxInput, FeatureToggle } from "../base";
-
-export const antag_tips: FeatureToggle = {
-  name: "Antagonist tips",
-  category: "GAMEPLAY",
-  description: "Gives a basic explanation on how to play an antagonist upon obtaining one.",
-  component: CheckboxInput,
-};
