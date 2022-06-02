1e2cbb6b8469155a7babf34478fce5c96327432c
JohnFulpWillard <53777086+JohnFulpWillard@users.noreply.github.com>
2022-06-02 07:13:54 -0400

    pubby + re-adds fulp tgui

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/antag_tips.tsx
@@ -0,0 +1,8 @@
+import { CheckboxInput, FeatureToggle } from "../base";
+
+export const antag_tips: FeatureToggle = {
+  name: "Antagonist tips",
+  category: "GAMEPLAY",
+  description: "Gives a basic explanation on how to play an antagonist upon obtaining one.",
+  component: CheckboxInput,
+};
