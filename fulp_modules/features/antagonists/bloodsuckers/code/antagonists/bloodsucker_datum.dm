6d957aa8624752791070c3cc7633c5a15314fa8c
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2022-07-26 07:54:54 -0400

    Bloodsucker tips

--- fulp_modules/features/antagonists/bloodsuckers/code/antagonists/bloodsucker_datum.dm
+++ fulp_modules/features/antagonists/bloodsuckers/code/antagonists/bloodsucker_datum.dm
@@ -10,7 +10,7 @@
 	hijack_speed = 0.5
 	hud_icon = 'fulp_modules/features/antagonists/bloodsuckers/icons/bloodsucker_icons.dmi'
 	ui_name = "AntagInfoBloodsucker"
-	tips = BLOODSUCKER_TIPS
+	antag_tips = BLOODSUCKER_TIPS
 	preview_outfit = /datum/outfit/bloodsucker_outfit
 
 	/// How much blood we have, starting off at default blood levels.
---
+++ tgui/packages/fulpui-patches/interfaces/TipsBloodsucker.tsx
@@ -0,0 +1,97 @@
+import { useBackend } from 'tgui/backend';
+import { Box, Flex, Divider, Section } from '../../tgui/components';
+import { resolveAsset } from '../../tgui/assets';
+import { Window } from '../../tgui/layouts';
+
+type Name = {
+  name: string;
+};
+
+export const TipsBloodsucker = (props, context) => {
+  const { act, data } = useBackend<Name>(context);
+  const { name } = data;
+  return (
+    <Window width={400} height={450} theme="spookyconsole">
+      <Window.Content>
+        <Section>
+          <Flex>
+            <Flex.Item>
+              <Box bold inline p={2}>
+                You are the {name}!
+              </Box>
+              <Box
+                position="absolute"
+                height="12rem"
+                as="img"
+                opacity={0.4}
+                src={resolveAsset(`bloodsucker.png`)}
+              />
+            </Flex.Item>
+          </Flex>
+          <Divider />
+          <Flex>
+            <Flex.Item>
+              You are a <b>Bloodsucker</b>, an undead blood-seeking monster living aboard Space Station 13.
+            </Flex.Item>
+            <Divider vertical />
+            <Flex.Item>
+              You regenerate your health slowly, you&#39;re weak to fire, and you depend on blood to survive. Don&#39;t allow your blood to run too low, or you&#39;ll enter a Frenzy!
+            </Flex.Item>
+          </Flex>
+          <Divider />
+          <Flex>
+            <Flex.Item>
+              Beware of your Humanity level! The more Humanity you lose, the easier it is to fall into a Frenzy!
+            </Flex.Item>
+            <Divider vertical />
+            <Flex.Item>
+              Avoid using your Feed ability while near others, or else you will risk <i>breaking the Masquerade</i>!
+            </Flex.Item>
+            <Divider />
+          </Flex>
+          <Divider />
+          <Flex>
+            <Flex.Item>
+              <Box
+                as="img"
+                height="20px"
+                src={resolveAsset(`day.png`)}
+              />
+              Fear the daylight! Solar flares will bombard the station periodically, and your coffin can guarantee your safety.
+            </Flex.Item>
+            <Divider vertical />
+            <Flex.Item>
+              <Box
+                as="img"
+                height="20px"
+                src={resolveAsset(`coffin.png`)}
+              />
+              Rest in a <b>Coffin</b> to claim it, and that area, as your lair.
+            </Flex.Item>
+          </Flex>
+          <Divider />
+          <Flex>
+            <Flex.Item>
+            Once you have a Coffin claimed, you will learn new structures in the <b>Tribal</b> section of your crafting menu.
+            </Flex.Item>
+            <Divider vertical />
+            <Flex.Item>
+              <Box
+                as="img"
+                height="20px"
+                src={resolveAsset(`vassalrack.png`)}
+              />
+              Examine your new structures to see how they function!
+            </Flex.Item>
+          </Flex>
+          <Divider />
+          <Flex>
+            <Flex.Item bold>
+            Other Bloodsuckers are not necessarily your friends, but your survival may depend on cooperation. Betray them at your own discretion and peril.
+            </Flex.Item>
+          </Flex>
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
