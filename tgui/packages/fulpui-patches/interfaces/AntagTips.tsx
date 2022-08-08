f5d5fb9d9ec9d745b3031f301a3db3599fcf0422
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2022-08-08 07:08:29 -0400

    prettierx

--- tgui/packages/fulpui-patches/interfaces/AntagTips.tsx
+++ tgui/packages/fulpui-patches/interfaces/AntagTips.tsx
@@ -1,7 +1,7 @@
 import { useBackend } from 'tgui/backend';
 import { classes } from 'common/react';
-import { Box, Flex, Stack, Divider, Section } from 'tgui/components';
-import { Window } from 'tgui/layouts';
+import { Box, Flex, Stack, Divider, Section } from '../../tgui/components';
+import { Window } from '../../tgui/layouts';
 
 type AntagTipInfo = {
   name: string;
@@ -30,20 +30,19 @@ export const AntagTips = (props, context) => {
           <Divider />
           <Stack mb={1}>
             <Stack.Item grow>
-            {(!antag_tips && 'None!') ||
-              antag_tips.map((antag_tip) => (
-                <>
-                  <Stack.Item key={antag_tip}>
-                    {antag_tip}
-                  </Stack.Item>
-                  <Divider />
-                </>
-              ))}
+              {(!antag_tips && 'None!') ||
+                antag_tips.map((antag_tip) => (
+                  <>
+                    <Stack.Item key={antag_tip}>{antag_tip}</Stack.Item>
+                    <Divider />
+                  </>
+                ))}
             </Stack.Item>
           </Stack>
           <Flex>
             <Flex.Item>
-              Don&#39;t be afraid to ask <b>mentors</b> for help using the mentorhelp button.
+              Don&#39;t be afraid to ask <b>mentors</b> for help using the
+              mentorhelp button.
             </Flex.Item>
           </Flex>
         </Section>
