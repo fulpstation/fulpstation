9878aac64ccfcd85b76dcd419c2867df880f24ef
SMOSMOSMOSMOSMO <95004236+SmoSmoSmoSmok@users.noreply.github.com>
2022-09-24 04:28:43 +0200

    dehardcodes the INFILRADIO UI (#820)
    * fsefsfsdf
    
    * Update infil_items.dm

--- tgui/packages/fulpui-patches/interfaces/InfilRadio.js
+++ tgui/packages/fulpui-patches/interfaces/InfilRadio.js
@@ -4,25 +4,12 @@ import { Window } from '../../tgui/layouts';
 
 export const InfilRadio = (props, context) => {
   const { act, data } = useBackend(context);
-  const { check, completed, dagd } = data;
+  const { check, completed, final } = data;
   return (
     <Window width={360} height={130} theme="hackerman">
       <Window.Content>
         <Section>
-          {(completed && !dagd && (
-            <Box>
-              You have proven yourself worthy of our final mission. The stereo
-              system in our shuttle was stolen, we need you to grand theft auto
-              their emergency escape shuttle for spares.
-            </Box>
-          )) ||
-            (completed && dagd && (
-              <Box>
-                Frankly, we have no more uses for you and we would prefer if you
-                removed all potential evidence of your existence. May you have
-                an everlasting glorious death.
-              </Box>
-            )) ||
+          {(completed && <Box> {final} </Box>) ||
             (data.check
               ? 'Your work has satisfied our investors.'
               : 'Prove your worth by completing an objective.')}
