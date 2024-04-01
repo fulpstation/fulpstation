71db82f419e30a3de55421c9824efdd3a53525dc
JohnFulpWillard <53777086+JohnFulpWillard@users.noreply.github.com>
2024-04-01 09:54:35 -0400

    .

--- dependencies.sh
+++ dependencies.sh
@@ -4,11 +4,8 @@
 #Final authority on what's required to fully build the project
 
 # byond version
-# Extracted from the Dockerfile. Change by editing Dockerfile's FROM command.
-LIST=($(sed -n 's/.*byond:\([0-9]\+\)\.\([0-9]\+\).*/\1 \2/p' Dockerfile))
-export BYOND_MAJOR=${LIST[0]}
-export BYOND_MINOR=${LIST[1]}
-unset LIST
+export BYOND_MAJOR=514
+export BYOND_MINOR=1589
 
 #node version
 export NODE_VERSION=14
