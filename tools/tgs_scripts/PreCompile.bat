78f51ab7080cfbc9c934898753e8ae517e1d0cb0
JohnFulpWillard <53777086+JohnFulpWillard@users.noreply.github.com>
2024-04-01 11:28:52 -0400

    alt

--- tools/tgs_scripts/PreCompile.bat
+++ tools/tgs_scripts/PreCompile.bat
@@ -1,10 +1,7 @@
 @echo off
 cd /D "%~dp0"
 set TG_BOOTSTRAP_CACHE=%cd%
-IF NOT %1 == "" (
-	rem TGS4+: we are passed the game directory on the command line
-	cd %1
-) ELSE IF EXIST "..\Game\B\tgstation.dmb" (
+IF EXIST "..\Game\B\tgstation.dmb" (
 	rem TGS3: Game/B/tgstation.dmb exists, so build in Game/A
 	cd ..\Game\A
 ) ELSE (
@@ -12,4 +9,4 @@ IF NOT %1 == "" (
 	cd ..\Game\B
 )
 set CBT_BUILD_MODE=TGS
-tools\build\build.bat
+tools\build\build
