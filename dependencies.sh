8daee1eb2c015f6c7c46c12a17560fe937ba5f46
JohnFulpWillard <53777086+JohnFulpWillard@users.noreply.github.com>
2024-04-01 10:56:43 -0400

    fuck you node

--- dependencies.sh
+++ dependencies.sh
@@ -9,13 +9,11 @@ export BYOND_MINOR=1589
 
 #node version
 export NODE_VERSION=14
-export NODE_VERSION_PRECISE=14.16.1
+export NODE_VERSION_LTS=20.12.0
+export NODE_VERSION_COMPAT=14.16.1
 
 #rust_g git tag
 export RUST_G_VERSION=3.1.0
-
-#node version
-export NODE_VERSION=12
 
 # PHP version
 export PHP_VERSION=7.2
