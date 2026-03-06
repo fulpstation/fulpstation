58860eaa655944fdd10e2d0bf6dbba67ac5b85f6
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2026-03-06 13:04:08 -0500

    Create antag_tips.tsx

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/antag_tips.tsx
@@ -0,0 +1,9 @@
+import { CheckboxInput, type FeatureToggle } from '../base';
+
+export const antag_tips: FeatureToggle = {
+  name: 'Antagonist tips',
+  category: 'GAMEPLAY',
+  description:
+    'Gives a basic explanation on how to play an antagonist upon obtaining one.',
+  component: CheckboxInput,
+};
